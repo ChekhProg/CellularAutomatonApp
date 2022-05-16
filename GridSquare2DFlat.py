8a214abae5da085cf5c54d327deaead7f65ce729
Chekh <chekhprogrammer@gmail.com>
2022-05-16 20:39:43 +0400

    refactoring coordinates, add interface elements

--- GridSquare2DFlat.py
+++ GridSquare2DFlat.py
@@ -9,11 +9,11 @@ from GridSquare2D import GridSquare2D
 
 class GridSquare2DFlat(GridSquare2D):
     def __init__(self, width, height):
-        width = width + 1
-        height = height + 1
+        width = width + 2
+        height = height + 2
         super().__init__(width, height)
         self.cells = np.zeros((height * width), dtype=bool)
-        self.colors = {False: Qt.GlobalColor.black, True: Qt.GlobalColor.darkYellow}
+        self.colors = {False: Qt.GlobalColor.black, True: Qt.GlobalColor.darkGreen}
 
     def set_random(self):
         for i in range(1, self.height - 1):
@@ -24,8 +24,8 @@ class GridSquare2DFlat(GridSquare2D):
     def clear(self):
         self.cells = np.zeros((self.height * self.width), dtype=bool)
 
-    def get_neighbors_moore(self, x, y):
-        pos = x * self.width + y
+    def get_neighbors_moore(self, i, j):
+        pos = i * self.width + j
         live_neighbors: int = 0
         if self.cells[pos - self.width - 1]: live_neighbors += 1
         if self.cells[pos - self.width]: live_neighbors += 1
@@ -39,15 +39,10 @@ class GridSquare2DFlat(GridSquare2D):
 
     def next_gen(self, n=1):
         def gen():
-            #neighbors_time = 0.0
-            #other_time = 0.0
             new_cells = np.zeros((self.height * self.width), dtype=bool)
             for i in range(1, self.height - 1):
                 for j in range(1, self.width - 1):
-                    #stime = time.time()
                     neighbors = self.get_neighbors_moore(i, j)
-                    #neighbors_time += time.time() - stime
-                    #stime = time.time()
                     pos = i * self.width + j
                     cur_status = self.cells[pos]
                     new_status = False
@@ -57,22 +52,16 @@ class GridSquare2DFlat(GridSquare2D):
                         if neighbors == 3 and not cur_status:
                             new_status = True
                     new_cells[pos] = new_status
-                    #other_time += time.time() - stime
-
             self.cells = new_cells
-            # print("Neighbors:", neighbors_time)
-            # print("Other:    ", other_time)
-            # print("Total:    ", neighbors_time+other_time)
-
-        for i in range(n):
+        for k in range(n):
             gen()
 
-    def getCellStatus(self, x, y):
-        pos = (x + 1) * self.width + (y + 1)
+    def getCellStatus(self, i, j):
+        pos = (i + 1) * self.width + (j + 1)
         return self.cells[pos]
 
-    def changeCellStatus(self, x, y):
-        pos = (x + 1) * self.width + (y + 1)
+    def changeCellStatus(self, i, j):
+        pos = (i + 1) * self.width + (j + 1)
         new_state = True
         if self.cells[pos]:
             new_state = False
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import time
 
 import numpy as np
 from PyQt6.QtCore import Qt, QTimer, QSize
-from PyQt6.QtGui import QBrush, QPen, QPalette, QColor
+from PyQt6.QtGui import QBrush, QPen, QPalette, QColor, QWindow
 from PyQt6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem, QApplication, QMainWindow, QWidget, \
-    QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy, QSlider, QLabel, QGridLayout
+    QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy, QSlider, QLabel, QGridLayout, QSpinBox, QLineEdit, QLayout
 
 from GridSquare2D import GridSquare2D
 from GridSquare2DFlat import GridSquare2DFlat
@@ -19,71 +19,74 @@ class CellView(QGraphicsRectItem):
         self.universe = universe
 
     def mousePressEvent(self, e):
-        new_state = self.universe.changeCellStatus(self.x, self.y)
-        print(new_state)
+        i = self.y
+        j = self.x
+        new_state = self.universe.changeCellStatus(i, j)
+        print("x: {}, y: {}".format(self.x, self.y))
         brush = QBrush(self.universe.colors[new_state])
         self.setBrush(brush)
 
 
 class UniverseView(QGraphicsView):
-    def __init__(self):
+    def __init__(self, main_widget):
         super().__init__()
+        self.cells = None
+        self.universe = None
         self.timer = QTimer()
         self.timer.timeout.connect(lambda: self.step())
+        self.main_widget = main_widget
 
-        rows = 50
+        rows = 40
         self.rows = rows
-        columns = 50
+        columns = 80
         self.columns = columns
         size = 10
         self.size = size
 
         self.scene = QGraphicsScene(0, 0, columns * size, rows * size)
 
-        # test
-        self.universe = GridSquare2DFlat(self.rows, self.columns)
-        cells = np.zeros((self.rows * self.columns), dtype=object)
-        for i in range(self.rows):
-            for j in range(self.columns):
-                cell = CellView(self.universe, self.size, i, j)
-                cell.setPos(i * self.size, j * self.size)
-                pos = i * self.columns + j
-                cells[pos] = cell
-                self.scene.addItem(cell)
-        self.cells = cells
+        self.universe = GridSquare2DFlat(self.columns, self.rows)
+        self.initCellsView()
         self.redrawUniverse()
-        # test
 
         self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         self.setScene(self.scene)
 
-    # def drawGrid(self):
-    #     print("!")
-    #     rows = self.rows
-    #     columns = self.columns
-    #     size = self.size
-    #     for x in range(rows):
-    #         self.scene.addLine(0, x * size, columns * size, x * size, QPen(Qt.GlobalColor.black))
-    #
-    #     for y in range(columns):
-    #         self.scene.addLine(y * size, 0, y * size, rows * size, QPen(Qt.GlobalColor.black))
+    def initCellsView(self):
+        cells = np.zeros((self.rows * self.columns), dtype=object)
+        for i in range(self.rows):
+            for j in range(self.columns):
+                x = j
+                y = i
+                cell = CellView(self.universe, self.size, x, y)
+                cell.setPos(x * self.size, y * self.size)
+                pos = y * self.columns + x
+                cells[pos] = cell
+                self.scene.addItem(cell)
+        self.cells = cells
+        print(self.cells.size)
+        print(self.rows)
+        print(self.columns)
+        self.redrawUniverse()
 
     def setCellViewColor(self, x, y, color):
-        pos = x * self.columns + y
+        pos = y * self.columns + x
         brush = QBrush(color)
         self.cells[pos].setBrush(brush)
 
     def redrawUniverse(self):
-        for i in range(self.columns):
-            for j in range(self.rows):
-                self.setCellViewColor(i, j, self.universe.colors[self.universe.getCellStatus(i, j)])
+        for i in range(self.rows):
+            for j in range(self.columns):
+                x = j
+                y = i
+                self.setCellViewColor(x, y, self.universe.colors[self.universe.getCellStatus(i, j)])
 
-    def step(self):
+    def step(self, n=1):
         stime = time.time()
-        self.universe.next_gen()
+        self.universe.next_gen(n)
         self.redrawUniverse()
         etime = time.time() - stime
-        print(etime)
+        #print(etime)
 
     def reset(self):
         self.universe.clear()
@@ -102,30 +105,96 @@ class UniverseView(QGraphicsView):
     def changeSpeed(self, i: float):
         self.timer.setInterval(1000 // int(i))
 
+    def changeRows(self, rows):
+        self.rows = rows
+        self.universe = GridSquare2DFlat(self.columns, self.rows)
+        self.changeSize(self.size)
+
+    def changeColumns(self, columns):
+        self.columns = columns
+        self.universe = GridSquare2DFlat(self.columns, self.rows)
+        self.changeSize(self.size)
+
+    def changeSize(self, size):
+        self.size = size
+        self.setFixedSize(self.columns * size + 4, self.rows * size + 4)
+        self.scene = QGraphicsScene(0, 0, self.columns * size, self.rows * size)
+        self.initCellsView()
+        self.setScene(self.scene)
+        self.main_widget.centralWidget().setFixedSize(self.main_widget.centralWidget().sizeHint())
+        self.main_widget.setFixedSize(self.main_widget.sizeHint())
+        self.redrawUniverse()
+
 
 class ToolBar(QWidget):
     def __init__(self, view):
         super().__init__()
+        self.setFixedWidth(150)
         layout = QVBoxLayout()
 
+        # Rows Horizontal Layout
+        rows_layout = QHBoxLayout()
+
+        lbl_rows = QLabel("Rows: ")
+        spinbox_rows = QSpinBox()
+        spinbox_rows.setRange(40, 200)
+        spinbox_rows.setValue(view.rows)
+        spinbox_rows.lineEdit().setDisabled(True)
+        spinbox_rows.valueChanged.connect(lambda x: view.changeRows(x))
+
+        rows_layout.addWidget(lbl_rows)
+        rows_layout.addWidget(spinbox_rows)
+        # End Rows Layout
+
+        # Rows Horizontal Layout
+        columns_layout = QHBoxLayout()
+
+        lbl_columns = QLabel("Columns: ")
+        spinbox_columns = QSpinBox()
+        spinbox_columns.setRange(40, 200)
+        spinbox_columns.setValue(view.columns)
+        spinbox_columns.lineEdit().setDisabled(True)
+        spinbox_columns.valueChanged.connect(lambda x: view.changeColumns(x))
+
+        columns_layout.addWidget(lbl_columns)
+        columns_layout.addWidget(spinbox_columns)
+        # End Rows Layout
+
+        # Size Horizontal Layout
+        size_layout = QHBoxLayout()
+
+        lbl_size = QLabel("Size: ")
+        spinbox_steps = QSpinBox()
+        spinbox_steps.setRange(1, 20)
+        spinbox_steps.setValue(view.size)
+        spinbox_steps.lineEdit().setDisabled(True)
+        spinbox_steps.valueChanged.connect(lambda s: view.changeSize(s))
+
+        size_layout.addWidget(lbl_size)
+        size_layout.addWidget(spinbox_steps)
+        # Size Step Layout
+
         btn_clear = QPushButton("Clear")
-        layout.addWidget(btn_clear)
         btn_clear.clicked.connect(lambda: view.reset())
 
         btn_rand = QPushButton("Randomize")
-        layout.addWidget(btn_rand)
         btn_rand.clicked.connect(lambda: view.randomize())
 
+        # Step Horizontal Layout
+        step_layout = QHBoxLayout()
+        spinbox_steps = QSpinBox()
+        spinbox_steps.setRange(1, 1000)
         btn_step = QPushButton("Step")
-        layout.addWidget(btn_step)
-        btn_step.clicked.connect(lambda: view.step())
+        btn_step.clicked.connect(lambda: view.step(spinbox_steps.value()))
+        step_layout.addWidget(btn_step)
+        step_layout.addWidget(spinbox_steps)
 
         btn_start = QPushButton("Run")
         btn_start.setCheckable(True)
         btn_start.clicked.connect(lambda c: view.runEvo(c))
+        # End Step Layout
 
         lbl_fps = QLabel()
-        #lbl_fps.setMaximumHeight(10)
         lbl_ms = QLabel()
 
         slider_speed = QSlider(Qt.Orientation.Horizontal)
@@ -139,11 +208,16 @@ class ToolBar(QWidget):
         lbl_fps.setText("FPS: " + str(slider_speed.value()))
         lbl_ms.setText("ms: " + str(1000 // slider_speed.value()))
 
+        layout.addLayout(rows_layout)
+        layout.addLayout(columns_layout)
+        layout.addLayout(size_layout)
+        layout.addWidget(btn_clear)
+        layout.addWidget(btn_rand)
+        layout.addLayout(step_layout)
         layout.addWidget(lbl_fps)
         layout.addWidget(lbl_ms)
         layout.addWidget(slider_speed)
         layout.addWidget(btn_start)
-
         layout.addStretch()
 
         self.setLayout(layout)
@@ -153,20 +227,38 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-
         self.setWindowTitle("GOL")
 
-        self.view = UniverseView()
+        self.view = UniverseView(self)
         self.tools = ToolBar(self.view)
 
         hbox = QHBoxLayout(self)
+        hbox.setSizeConstraint(QLayout.SizeConstraint.SetMaximumSize)
 
-        hbox.addWidget(self.view)
         hbox.addWidget(self.tools)
+        hbox.addWidget(self.view)
 
         widget = QWidget()
         widget.setLayout(hbox)
+
+        # widget.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+
         self.setCentralWidget(widget)
+
+        # widget.setAutoFillBackground(True)
+        # palette = widget.palette()
+        # palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.red)
+        # widget.setPalette(palette)
+        #
+        # self.tools.setAutoFillBackground(True)
+        # palette = widget.palette()
+        # palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.green)
+        # self.tools.setPalette(palette)
+        #
+        # self.view.setAutoFillBackground(True)
+        # palette = widget.palette()
+        # palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.blue)
+        # self.view.setPalette(palette)
 
 
 app = QApplication(sys.argv)
